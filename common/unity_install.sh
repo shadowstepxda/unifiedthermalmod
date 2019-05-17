dda6190a055ab7af26dff68249a179c72e05fd38
nipunnarang <nipunnarang12@gmail.com>
2019-05-17 21:00:38 +0530

    Updated to Unity v4.2
    Updated the base template to Unity v4.2 and miscellaneous cleanup

--- common/unity_install.sh
+++ common/unity_install.sh
@@ -1,7 +1,30 @@
+#Copy appropriate thermal configuration file based on detected android version
 if [ $API -eq 23 ]; then
-  cp -f $TMPDIR/marshmallow/thermal-engine.conf $TMPDIR/system/etc/thermal-engine.conf
+  ui_print " "
+  ui_print "- Android version detected: Marshmallow"
+  ui_print " "
+  ui_print "- Copying appropriate thermal configuration"
+  ui_print " "
+  cp -f $TMPDIR/conf/marshmallow/thermal-engine.conf $TMPDIR/system/etc/thermal-engine.conf
+elif [ $API -eq 24 ] || [ $API -eq 25 ]; then
+  ui_print " "
+  ui_print "- Android version detected: Nougat"
+  ui_print " "
+  ui_print "- Copying appropriate thermal configuration"
+  ui_print " "
+  cp -f $TMPDIR/conf/nougat/thermal-engine.conf $TMPDIR/system/etc/thermal-engine.conf
+elif [ $API -eq 26 ] || [ $API -eq 27 ]; then
+  ui_print " "
+  ui_print "- Android version detected: Oreo"
+  ui_print " "
+  ui_print "- Copying appropriate thermal configuration"
+  ui_print " "
+  cp -f $TMPDIR/conf/oreo/thermal-engine.conf $TMPDIR/system/etc/thermal-engine.conf
 else
-  if [ $API -eq 24 ] || [ $API -eq 25 ]; then
-    cp -f $TMPDIR/nougat/thermal-engine.conf $TMPDIR/system/etc/thermal-engine.conf
-  fi
+  ui_print " "
+  ui_print "- Android version detected: Pie"
+  ui_print " "
+  ui_print "- Copying appropriate thermal configuration"
+  ui_print " "
+  cp -f $TMPDIR/conf/pie/thermal-engine.conf $TMPDIR/system/etc/thermal-engine.conf
 fi
